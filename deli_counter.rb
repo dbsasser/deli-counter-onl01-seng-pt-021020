46c02a9d2492a992653367ecb8dc472c0710239d
Daniel Sasser <danielbsasser@gmail.com>
2020-02-12 03:28:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,15 @@
 katz_deli = []
 
 def line(array)
+  
   line_number = 1
+  
+  numbered_line = [] 
+  
   if array.length == 0 
     puts "The line is currently empty."
   else
-    array.each
+    array.each do 
   end
   
 end
